63349099a5304f636a470052054e026e9231da48
i.vukolov <i.vukolov@vrgaz.ru>
2025-02-18 16:36:31 +0300

    added auth path

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -5,7 +5,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('pages/', include('pages.urls'))
+    path('auth/', include('django.contrib.auth.urls')),
+    path('pages/', include('pages.urls')),
 ]
 
 if settings.DEBUG:
