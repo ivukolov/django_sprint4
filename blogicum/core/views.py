48d95002588ea3e927fb6ef1b178736a28b75d59
i.vukolov <i.vukolov@vrgaz.ru>
2025-02-19 12:19:29 +0300

    added custom eroors 403, 404, 500

---
+++ blogicum/core/views.py
@@ -0,0 +1,14 @@
+# core/views.py
+from django.shortcuts import render
+
+
+def internal_server_error(request):
+    return render(request, 'pages/500.html', status=500)
+
+def page_not_found(request, exception):
+    return render(request, 'pages/404.html', status=404)
+
+
+def csrf_failure(request, reason=''):
+    return render(request, 'pages/403csrf.html', status=403)
+
