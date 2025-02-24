f15b9d96541cb9b01ef68645c0d1585773ef8f29
i.vukolov <i.vukolov@vrgaz.ru>
2025-02-24 16:08:42 +0300

    added to success_url  reverse_lazy

--- blogicum/users/views.py
+++ blogicum/users/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from django.conf import settings
+from django.urls import reverse_lazy
 
 from .forms import BlogicumUserCreationForm
 
@@ -8,4 +8,4 @@ from .forms import BlogicumUserCreationForm
 class BlogicumCreateView(CreateView):
     template_name = 'registration/registration_form.html'
     form_class = BlogicumUserCreationForm
-    success_url = settings.LOGIN_REDIRECT_URL
+    success_url = reverse_lazy(settings.LOGIN_REDIRECT_URL)
