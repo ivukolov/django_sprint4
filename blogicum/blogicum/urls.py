ade94e539e574fdb727ce10623a3423287869b06
i.vukolov <i.vukolov@vrgaz.ru>
2025-02-24 15:30:45 +0300

    added urlpatterns for MEDIA_URL

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 
 handler404 = settings.HANDLER_404
 handler500 = settings.HANDLER_500
@@ -15,3 +16,6 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    ) 
