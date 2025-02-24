f8c781a10fa4fa253c258e3a38406e83a36ea404
i.vukolov <i.vukolov@vrgaz.ru>
2025-02-24 16:08:23 +0300

    remove comments

--- blogicum/users/forms.py
+++ blogicum/users/forms.py
@@ -7,8 +7,6 @@ User = get_user_model()
 
 
 class BlogicumUserCreationForm(UserCreationForm):
-    # Наследуем класс Meta от соответствующего класса родительской формы.
-    # Так этот класс будет не перезаписан, а расширен.
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ('first_name', 'last_name', 'username', 'email',)
