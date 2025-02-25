8017c6fac90df3688d6eda0d9ac32e0b0ef89fe8
i.vukolov <i.vukolov@vrgaz.ru>
2025-02-25 13:32:07 +0300

    edited view errors

--- blogicum/core/views.py
+++ blogicum/core/views.py
@@ -1,8 +1,9 @@
 # core/views.py
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
-def internal_server_error(request):
+def custom_500(request):
     return render(request, 'pages/500.html', status=500)
 
 
@@ -12,3 +13,28 @@ def page_not_found(request, exception):
 
 def csrf_failure(request, reason=''):
     return render(request, 'pages/403csrf.html', status=403)
+
+
+# class Custom404View(TemplateView):
+#     template_name = 'pages/404.html'
+
+#     def get(self, request, *args, **kwargs):
+#         context = self.get_context_data(**kwargs)
+#         return self.render_to_response(context, status=404)
+
+
+# class Custom500View(TemplateView):
+#     template_name = 'pages/500.html'
+
+#     def get(self, request, *args, **kwargs):
+#         context = self.get_context_data(**kwargs)
+#         return self.render_to_response(context, status=500)
+
+
+# class Custom403View(TemplateView):
+#     template_name = 'pages/403.html'
+
+#     def get(self, request, *args, **kwargs):
+#         context = self.get_context_data(**kwargs)
+#         return self.render_to_response(context, status=403)
+
