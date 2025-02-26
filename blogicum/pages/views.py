eec6456c0a1bcb1a63fc7437239d31bcccefc618
i.vukolov <i.vukolov@vrgaz.ru>
2025-02-26 09:45:30 +0300

    added error pages

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
 
 
 class About(TemplateView):
@@ -9,3 +10,15 @@ class About(TemplateView):
 class Rules(TemplateView):
 
     template_name = 'pages/rules.html'
+
+
+def custom_500(request):
+    return render(request, 'pages/500.html', status=500)
+
+
+def page_not_found(request, exception):
+    return render(request, 'pages/404.html', status=404)
+
+
+def csrf_failure(request, reason=''):
+    return render(request, 'pages/403csrf.html', status=403)
