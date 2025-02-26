1f4b8464aedcad22a4c57cf41304ee38aab0e4fa
i.vukolov <i.vukolov@vrgaz.ru>
2025-02-26 09:45:57 +0300

    removed django.auth path

--- blogicum/users/urls.py
+++ blogicum/users/urls.py
@@ -4,7 +4,6 @@ from django.views.generic.edit import CreateView
 from . import views
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
     path(
         'registration/',
         views.BlogicumCreateView.as_view(),
