1f9435b6cf1483f4826457cdd7652c993093be2d
i.vukolov <i.vukolov@vrgaz.ru>
2025-02-26 12:14:40 +0300

    Pep Fix

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -90,7 +90,9 @@ class Comment(models.Model):
     text = models.TextField('Текст коментария')
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments'
+    )
 
     class Meta:
         ordering = ('created_at',)
