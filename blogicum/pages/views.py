984bd89459cd26dd160d0b6c2ff3bedd633dce75
i.vukolov <i.vukolov@vrgaz.ru>
2025-02-27 12:48:49 +0300

    edited error code status to CONST

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render
+from http import HTTPStatus
 
 
 class About(TemplateView):
@@ -13,12 +14,18 @@ class Rules(TemplateView):
 
 
 def custom_500(request):
-    return render(request, 'pages/500.html', status=500)
+    return render(
+        request, 'pages/500.html', status=HTTPStatus.INTERNAL_SERVER_ERROR
+    )
 
 
 def page_not_found(request, exception):
-    return render(request, 'pages/404.html', status=404)
+    return render(
+        request, 'pages/404.html', status=HTTPStatus.NOT_FOUND
+    )
 
 
 def csrf_failure(request, reason=''):
-    return render(request, 'pages/403csrf.html', status=403)
+    return render(
+        request, 'pages/403csrf.html', status=HTTPStatus.FORBIDDEN
+    )
