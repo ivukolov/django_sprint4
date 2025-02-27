2324eb9b50cd8c7e884b8121f7986ba8078f7dc1
i.vukolov <i.vukolov@vrgaz.ru>
2025-02-27 12:42:07 +0300

    error correction: remove django comments, move down user apps

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -3,11 +3,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-+vd_cums4@6iz^%s74*swn#(ib@d2rvje19udwn5s*j6legvaq'
 
 # Коснтанты постов:
@@ -17,7 +12,6 @@ MAX_HEAD_LENGHT = 256
 # Лимит постов для выборки на главной странице.
 MAX_POSTS_LIMIT = 10
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
@@ -25,18 +19,15 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
 ]
 
-# Параметры страниц с ошибками
+# Параметры страниц с ошибками.
 
 CSRF_FAILURE_VIEW = 'pages.views.csrf_failure'
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-# Application definition
+
+# Application definition.
 
 INSTALLED_APPS = [
-    'core.apps.CoreConfig',
-    'blog.apps.BlogConfig',
-    'users.apps.UsersConfig',
-    'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,6 +35,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core.apps.CoreConfig',
+    'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
+    'pages.apps.PagesConfig',
     'debug_toolbar',
 ]
 
@@ -84,10 +79,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'blogicum.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -95,9 +86,7 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+# Password validation.
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -114,9 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
+# Internationalization.
 
 LANGUAGE_CODE = 'ru-RU'
 
@@ -128,21 +115,18 @@ USE_L10N = True
 
 USE_TZ = True
 
+# БЛОК СВЯЗАННЫЙ С РЕГИСТРАЦИЕЙ ПОЛЬЗОВАТЕЛЕЙ.
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-# БЛОК СВЯЗАННЫЙ С РЕГИСТРАЦИЕЙ ПОЛЬЗОВАТЕЛЕЙ
-
-# URL по умолчанию для аутентификации пользователя
+# URL по умолчанию для аутентификации пользователя.
 LOGIN_URL = 'login'
 
-# Редирект по умолчанию после авторизации
+# Редирект по умолчанию после авторизации.
 LOGIN_REDIRECT_URL = 'blog:index'
+
+# Параметры для статических ресурсов
 
 STATIC_URL = '/static/'
 
-# Параметры для статических ресурсов
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
@@ -154,6 +138,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
