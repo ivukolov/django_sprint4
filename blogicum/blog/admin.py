6e970f56c94e44734e2df196c3eb940a139000cb
i.vukolov <i.vukolov@vrgaz.ru>
2025-02-28 09:19:09 +0300

    added admin fields

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,8 +1,60 @@
 from django.contrib import admin
 
 
-from .models import Post, Category, Location
+from .models import Post, Category, Location, Comment
 
-admin.site.register(Post)
-admin.site.register(Category)
-admin.site.register(Location)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'pub_date',
+        'author',
+        'is_published',
+        'category',
+        'created_at',
+    )
+    list_editable = (
+        'pub_date',
+        'is_published',
+        'category',
+    )
+    search_fields = ('title',)
+    list_filter = ('category',)
+    list_display_links = ('title',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'slug',
+        'is_published',
+    )
+    search_fields = ('title',)
+    list_filter = ('is_published',)
+    list_display_links = ('title',)
+
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'is_published',
+    )
+    search_fields = ('name',)
+    list_filter = ('is_published',)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'text',
+        'created_at',
+        'author',
+        'post',
+    )
+    search_fields = ('text',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Comment, CommentAdmin)
